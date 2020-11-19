ae443e40a8894ae9585ffe7ee2d03a14033cc56b
Kenneth Jessen <kennethjessen1@hotmail.com>
2020-11-19 08:47:02 +0100

    Add project files.

---
+++ CookieClicker/Data/CookieClickerData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieClicker.Data
+{
+    public class CookieClickerData
+    {
+        public double points { get; set; } = 0;
+        public bool isAuto = false;
+
+        public double pointsPerSeconds { get; set; } = 0;
+
+        public int upgrade { get; set; } = 1;
+        public double upgradeCostFirst { get; set; } = 20;
+        public double upgradeCostSecond { get; set; } = 50;
+    }
+}
---
+++ CookieClicker/Data/CookieUpgrades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieClicker.Data
+{
+    public class CookieUpgrades
+    {
+        public string UpgradeName { get; set; }
+        public double UpgradeCost { get; set; }
+        public int UpgradeMultiplier { get; set; }
+    }
+}
